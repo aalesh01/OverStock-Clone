b58166ee3d8465e4a5446e9ebbe58f0a0cb0e750
aalesh01 <aalesh111999@gmail.com>
2022-04-15 05:54:25 +0530

    total in cart.html

--- cart.js
+++ cart.js
@@ -1,7 +1,9 @@
 if (localStorage.getItem("itemsInCart") === null) var cartItems = [];
 else cartItems = JSON.parse(localStorage.getItem("itemsInCart"));
 
-cartItems.forEach((element,index) => {
+var totalPrice = 0;
+var subTotalPrice = 0;
+cartItems.forEach((element, index) => {
     var item = document.createElement("div");
 
     var image = document.createElement("img");
@@ -15,12 +17,16 @@ cartItems.forEach((element,index) => {
     var strikText = document.createElement("p");
     strikText.innerText = "INR " + element.price;
     strikText.setAttribute("id", "strike");
+    totalPrice += +(element.price);
+
 
     var price = document.createElement("p");
-    price.innerText = "Sale INR " + (element.price / 0.20);
+    price.innerText = "Sale INR " + (element.price / 1.5);
     price.setAttribute("id", "price")
+    subTotalPrice += +(element.price / 1.5);
 
     var dropdownBT = document.createElement("select");
+    
     for (var i = 1; i <= 20; i++) {
         var option = document.createElement("option");
         option.setAttribute("value", i);
@@ -30,10 +36,10 @@ cartItems.forEach((element,index) => {
 
     var removeBT = document.createElement("button");
     removeBT.innerText = "Remove";
-    removeBT.setAttribute("id","removeBT")
-    removeBT.addEventListener("click",function(){
-        cartItems.splice(index,1);
-        localStorage.setItem("itemsInCart",JSON.stringify(cartItems));
+    removeBT.setAttribute("id", "removeBT")
+    removeBT.addEventListener("click", function () {
+        cartItems.splice(index, 1);
+        localStorage.setItem("itemsInCart", JSON.stringify(cartItems));
         document.location.reload();
     })
 
@@ -41,3 +47,37 @@ cartItems.forEach((element,index) => {
     item.append(image, itemDetails);
     document.getElementById("pList").append(item);
 });
+var table = document.createElement("table");
+var row1 = document.createElement('tr');
+
+var items = document.createElement('td');
+items.innerText = "(" + cartItems.length + ") Items ";
+
+var totalP = document.createElement('td');
+totalP.innerText = "INR " + totalPrice
+
+var row2p5 = document.createElement('tr');
+
+var text2 = document.createElement('td');
+text2.innerText = "Discount";
+
+var discount = document.createElement('td');
+discount.innerText = "INR " + (totalPrice - subTotalPrice);
+
+
+var row2 = document.createElement('tr');
+
+var text1 = document.createElement('td');
+text1.innerText = "Your Total:";
+
+var subTotalP = document.createElement('td');
+subTotalP.innerText = "INR " + subTotalPrice;
+
+row1.append(items, totalP);
+row2p5.append(text2, discount);
+row2.append(text1, subTotalP);
+
+table.append(row1, row2p5, row2)
+document.getElementById("totalAmount").append(table)
+
+
